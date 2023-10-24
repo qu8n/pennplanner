ddeb3386e5743cebbbb189138853beda875c655a
Quan Nguyen <86090707+qu8n@users.noreply.github.com>
2023-10-24 02:18:04 -0400

    Add layouts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,9 +5,22 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className={`${inter.className}`}>
+    <main className={`flex flex-col h-screen ${inter.className}`}>
       <Navbar />
-      Hello
+      <div className="flex flex-1 overflow-hidden">
+        {/* Sidebar */}
+        <aside className="flex w-[35rem] p-4 ring"></aside>
+
+        <div className="flex flex-1 flex-col ring">
+          {/* Header */}
+          <div className="flex h-16 p-4 ring"></div>
+
+          {/* Scrollable content */}
+          <div className="flex flex-1 overflow-y-auto paragraph px-4 ring"></div>
+        </div>
+      </div>
+
+      <footer className="flex ring">Footer</footer>
     </main>
   )
 }
