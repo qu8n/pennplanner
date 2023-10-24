91b8a0acc5970141069ba0a6d8067af335080f35
Quan Nguyen <86090707+qu8n@users.noreply.github.com>
2023-10-24 14:19:35 -0400

    Add a schedule component

---
+++ src/components/Schedule.tsx
@@ -0,0 +1,50 @@
+export function Schedule() {
+  return (
+    <>
+      <div className="w-full p-4 rounded-xl flex flex-col gap-2 ring-2 ring-gray-300">
+        <p className="font-bold text-xl ml-2">2023</p>
+        <div className="grid grid-cols-3 gap-4">
+          <div className="bg-gray-100 rounded-lg p-4 flex flex-col gap-y-2 h-72">
+            <p className="font-semibold text-lg">Spring</p>
+            <div className="bg-white ring-2 ring-gray-200 rounded-lg p-3">
+              CIS 5150
+            </div>
+            <div className="bg-white ring-2 ring-gray-200 rounded-lg p-3">
+              CIS 5150
+            </div>
+            <div className="bg-white ring-2 ring-gray-200 rounded-lg p-3">
+              CIS 5150
+            </div>
+            <div className="bg-white ring-2 ring-gray-200 rounded-lg p-3">
+              CIS 5150
+            </div>
+          </div>
+          <div className="bg-gray-100 rounded-lg p-4 flex flex-col gap-y-2 h-72">
+            <p className="font-semibold text-lg">Summer</p>
+            <div className="bg-white ring-2 ring-gray-200 rounded-lg p-3">
+              CIS 5150
+            </div>
+            <div className="bg-white ring-2 ring-gray-200 rounded-lg p-3">
+              CIS 5150
+            </div>
+          </div>
+          <div className="bg-gray-100 rounded-lg p-4 flex flex-col gap-y-2 h-72">
+            <p className="font-semibold text-lg">Fall</p>
+            <div className="bg-white ring-2 ring-gray-200 rounded-lg p-3">
+              CIS 5150
+            </div>
+            <div className="bg-white ring-2 ring-gray-200 rounded-lg p-3">
+              CIS 5150
+            </div>
+            <div className="bg-white ring-2 ring-gray-200 rounded-lg p-3">
+              CIS 5150
+            </div>
+            <div className="bg-white ring-2 ring-gray-200 rounded-lg p-3">
+              CIS 5150
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,9 @@ import { Inter } from 'next/font/google'
 import { Navbar } from '@/components/Navbar'
 import { Sidebar } from '@/components/Sidebar'
 import { Toolbar } from '@/components/Toolbar'
+import { Schedule } from '@/components/Schedule'
+import { Divider } from '@nextui-org/react'
+import { PlusCircleIcon } from '@heroicons/react/24/outline'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,18 +13,22 @@ export default function Home() {
     <main className={`flex flex-col h-screen ${inter.className}`}>
       <Navbar />
       <div className="flex flex-1 overflow-hidden">
-        <aside className="flex-col flex w-[30rem] p-4 ring">
+        <aside className="flex-col flex w-[30rem] p-4">
           <Sidebar />
         </aside>
 
-        <div className="flex flex-1 flex-col ring">
-          {/* Header */}
-          <div className="flex flex-row items-center h-16 p-4 ring gap-2">
+        <div className="flex flex-1 flex-col">
+          <div className="flex flex-row items-center h-16 p-4 gap-2">
             <Toolbar />
           </div>
 
-          {/* Scrollable content */}
-          <div className="flex flex-1 overflow-y-auto paragraph px-4 ring"></div>
+          <Divider />
+
+          <div className="flex flex-col overflow-y-auto p-4 gap-4">
+            <Schedule />
+            <Schedule />
+            <Schedule />
+          </div>
         </div>
       </div>
     </main>
