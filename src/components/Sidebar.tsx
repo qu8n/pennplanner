e8e61d8fbc72a46fc6e0dda8250511347a189503
Quan Nguyen <86090707+qu8n@users.noreply.github.com>
2023-10-25 10:48:00 -0400

    Add MCIT Central stats data and display them inside sidebar items

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -7,6 +7,7 @@ import {
 import { Button, Input, Accordion, AccordionItem } from '@nextui-org/react'
 import { useState } from 'react'
 import { courses } from '@/data/courses'
+import { Rating } from '@smastrom/react-rating'
 
 const accordionItemClasses = {
   base: 'ring-2 ring-gray-300 mb-3 rounded-md',
@@ -80,16 +81,57 @@ export function Sidebar() {
                 subtitle={course.course_name}
               >
                 <div>
+                  <p className="font-semibold">MCITCentral Stats</p>
+                  <div className="flex flex-row gap-2">
+                    <span className="mr-[1.3rem] font-medium">Rating:</span>
+                    <Rating
+                      readOnly={true}
+                      style={{ maxWidth: 100 }}
+                      value={course.avg_rating ?? 0}
+                    />
+                    {course.avg_rating ?? ''}
+                    {course.review_count
+                      ? ` (${course.review_count} reviews)`
+                      : 'N/A'}
+                  </div>
+                  <div className="flex flex-row gap-2">
+                    <span className="font-medium mr-[0.2rem]">Difficulty:</span>
+                    <Rating
+                      readOnly={true}
+                      style={{ maxWidth: 100 }}
+                      value={course.avg_difficulty ?? 0}
+                    />
+                    {course.avg_difficulty ?? 'N/A'}
+                  </div>
+                  <div className="flex flex-row gap-2">
+                    <span className="font-medium">Workload: </span>
+                    <Rating
+                      readOnly={true}
+                      style={{ maxWidth: 100 }}
+                      value={
+                        course.avg_hours_per_week
+                          ? course.avg_hours_per_week > 20
+                            ? 5
+                            : course.avg_hours_per_week / 4
+                          : 0
+                      }
+                    />
+                    {course.avg_hours_per_week
+                      ? `${course.avg_hours_per_week} hours/week`
+                      : 'N/A'}
+                  </div>
+                </div>
+                <div>
+                  <p className="font-semibold">Course Unit</p>
+                  <p>{course.course_unit} unit</p>
+                </div>
+                <div>
                   <p className="font-semibold">Description</p>
                   <p>{course.course_description}</p>
                 </div>
                 <div>
                   <p className="font-semibold">Pre-Requisites</p>
                   <p>{course.course_prereqs}</p>
-                </div>
-                <div>
-                  <p className="font-semibold">Course Units</p>
-                  <p>{course.course_unit} unit</p>
                 </div>
               </AccordionItem>
             ))}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { NextUIProvider } from '@nextui-org/react'
+import '@smastrom/react-rating/style.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
