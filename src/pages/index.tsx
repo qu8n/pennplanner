3e7df85904fff5716b5c8b7b856e543d7dcd15b6
Quan Nguyen <86090707+qu8n@users.noreply.github.com>
2023-11-22 21:00:58 -0500

    Animate the program name on landing page and add a chip on lack of mobile support

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,44 @@
 import { Navbar } from '@/components/Navbar'
 import { AcademicCapIcon } from '@heroicons/react/24/outline'
-import { Button, Link } from '@nextui-org/react'
-import { motion } from 'framer-motion'
+import { Button, Chip, Link } from '@nextui-org/react'
 import { useRouter } from 'next/router'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { AnimatePresence, motion } from 'framer-motion'
+
+const PROGRAM_NAMES = ['MCIT', 'MSE-DS']
+const framerVariants = {
+  enter: () => {
+    return {
+      y: -20,
+      opacity: 0,
+    }
+  },
+  center: {
+    zIndex: 1,
+    y: 0,
+    opacity: 1,
+  },
+  exit: () => {
+    return {
+      zIndex: 0,
+      opacity: 0,
+    }
+  },
+}
 
 export default function Home() {
   const router = useRouter()
+  const [programNameIndex, setProgramNameIndex] = useState(0)
+
+  useEffect(() => {
+    setTimeout(() => {
+      let nextIndex = programNameIndex + 1
+      if (nextIndex === PROGRAM_NAMES.length) {
+        nextIndex = 0
+      }
+      setProgramNameIndex(nextIndex)
+    }, 2 * 1000)
+  }, [programNameIndex])
 
   return (
     <>
@@ -18,9 +50,40 @@ export default function Home() {
         transition={{ duration: 2, ease: 'easeOut' }}
         className="m-auto flex w-full flex-col items-center text-center"
       >
-        <h1 className="mt-4 text-3xl font-bold text-blue-700">
-          Easily plan out your entire online MCIT journey.
-        </h1>
+        <Chip variant="dot" color="danger" className="text-neutral-500">
+          Not currently supported on mobile
+        </Chip>
+
+        <div className="mt-4 flex flex-col text-left text-3xl text-blue-700">
+          <span className="mr-2 font-medium">
+            Visualize your entire journey
+          </span>
+          <div>
+            <span
+              className={`mr-2 font-medium ${
+                programNameIndex === 0 ? 'ml-2' : '-ml-3'
+              }`}
+            >
+              in the online
+            </span>
+            <AnimatePresence>
+              <motion.span
+                className="fixed font-semibold"
+                variants={framerVariants}
+                key={programNameIndex}
+                initial="enter"
+                animate="center"
+                exit="exit"
+                transition={{
+                  y: { type: 'spring', stiffness: 500, damping: 100 },
+                  opacity: { duration: 0.5 },
+                }}
+              >
+                {PROGRAM_NAMES[programNameIndex]} program
+              </motion.span>
+            </AnimatePresence>
+          </div>
+        </div>
 
         <motion.div
           initial={{ opacity: 0, y: 100 }}
@@ -29,7 +92,7 @@ export default function Home() {
         >
           <h2 className="mt-2 max-w-md text-lg text-neutral-500">
             Compare courses, plan out your semesters, and track your progress
-            towards graduation all in one place.
+            towards graduation -- all in one place
           </h2>
         </motion.div>
 
@@ -58,7 +121,7 @@ export default function Home() {
             autoPlay
             muted
             loop
-            className="m-auto mt-10 max-w-3xl rounded-lg border-5 border-gray-100 shadow-md ring-1 ring-neutral-300"
+            className="m-auto mt-10 rounded-lg border-5 border-gray-100 p-2 shadow-md ring-1 ring-neutral-300 lg:max-w-3xl"
           >
             <source src="/demo.mp4" type="video/mp4" />
           </video>
