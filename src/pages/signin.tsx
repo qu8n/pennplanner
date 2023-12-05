7c3f92e85f0fc81811d3f2a445f250526f8765b5
Quan Nguyen <86090707+qu8n@users.noreply.github.com>
2023-12-05 08:45:33 -0500

    Show a loading toast after submitting the sign up form

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -39,7 +39,8 @@ export default function SignIn() {
     first_year: new Date().getFullYear(),
     program: 'MCIT',
   })
-  const [showLoading, setShowLoading] = useState(true)
+  const [showPageLoading, setShowPageLoading] = useState(true)
+  const [showSubmitLoading, setShowSubmitLoading] = useState(false)
 
   useEffect(() => {
     async function getUsername() {
@@ -67,12 +68,12 @@ export default function SignIn() {
         if (username) {
           router.push(`/${username}`)
         } else {
-          setShowLoading(false)
+          setShowPageLoading(false)
           setShowSignUpForm(true)
         }
       })
     } else {
-      setShowLoading(false)
+      setShowPageLoading(false)
     }
     // eslint-disable-next-line
   }, [user, isLoading])
@@ -87,7 +88,7 @@ export default function SignIn() {
       <Navbar maxWidthSize="xl" twHeight="h-20" twTextSize="text-sm" />
 
       <div className="flex flex-1 justify-evenly px-32">
-        {showLoading ? (
+        {showPageLoading ? (
           <div className="m-auto flex flex-col items-center">
             <ProgressBar
               height="150"
@@ -316,7 +317,10 @@ export default function SignIn() {
                 </RadioGroup>
 
                 <Button
+                  isDisabled={showSubmitLoading}
                   onPress={async () => {
+                    setShowSubmitLoading(true)
+
                     const allFilledOut = Object.values(newUser).every(
                       (value) => value !== '',
                     )
@@ -327,6 +331,8 @@ export default function SignIn() {
                           background: '#fecaca',
                         },
                       })
+
+                      setShowSubmitLoading(false)
                       return
                     }
 
@@ -348,17 +354,24 @@ export default function SignIn() {
                               : [],
                         },
                       ])
+
                     if (userCreateError) {
                       console.error('userCreateError:', userCreateError)
 
-                      if (userCreateError.code === '23505') {
-                        toast.error('Username is already taken', {
+                      toast.error(
+                        `${
+                          userCreateError.code === '23505'
+                            ? 'Username is already taken'
+                            : userCreateError.message
+                        }`,
+                        {
                           style: {
                             background: '#fecaca',
                           },
-                        })
-                      }
+                        },
+                      )
 
+                      setShowSubmitLoading(false)
                       return
                     } else {
                       router.push(`/${newUser.username}`)
@@ -366,8 +379,32 @@ export default function SignIn() {
                   }}
                   className="custom-gradient mt-6 w-full rounded-md text-white"
                 >
-                  Confirm
+                  Submit
                 </Button>
+
+                {showSubmitLoading && (
+                  <div className="mx-auto flex flex-row items-center gap-2">
+                    <svg
+                      aria-hidden="true"
+                      className="h-4 w-4 animate-spin fill-blue-600 text-gray-200 dark:text-gray-600"
+                      viewBox="0 0 100 101"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                        fill="currentColor"
+                      />
+                      <path
+                        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                        fill="currentFill"
+                      />
+                    </svg>
+                    <span className="text-xs text-neutral-500">
+                      Creating your planner...
+                    </span>
+                  </div>
+                )}
               </div>
             </div>
 
